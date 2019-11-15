c1a83f69a56343ca6ef7bd716f37c41717c79056
Jacob Craiglow <craiglowj16@students.ecu.edu>
2019-11-15 14:44:35 -0500

    Fixing Merge Conflicts -Attempt 2

--- app/src/main/java/com/pizza_pi/MainActivity.java
+++ app/src/main/java/com/pizza_pi/MainActivity.java
@@ -1,25 +1,19 @@
 package com.pizza_pi;
 
-<<<<<<< HEAD
-import android.content.Intent;
-=======
->>>>>>> master
-import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-<<<<<<< HEAD
-import android.widget.Button;
-import android.widget.Toast;
-=======
->>>>>>> master
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,12 +21,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
+
 
         Button mPickButton = findViewById(R.id.pick);
         Button mHelpButton = findViewById(R.id.help);
-=======
->>>>>>> master
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -44,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-<<<<<<< HEAD
 
         //pick button is clicked launch calc page 
         mPickButton.setOnClickListener(new View.OnClickListener() {
@@ -67,12 +59,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    
 
-
-=======
-    }
-
->>>>>>> master
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -87,11 +75,8 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-<<<<<<< HEAD
-        //noinspection SimplifiableIfStatement 
-=======
         //noinspection SimplifiableIfStatement
->>>>>>> master
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
